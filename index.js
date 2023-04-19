65b5606d9025a7868a0b5db9646bba19a61631b6
Tugba Esat <tugbaesat007@gmail.com>
2023-04-19 11:55:21 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // code your solution here
+
+function superbowlWin(arr) {
+  const winResults = arr.find((item) => {
+    return item.result === "W";
+  });
+  if (winResults) {
+      return winResults.year;
+  }
+
+}
+
+
+
+
+
+
+
+
+
